a8361996628630481752f607c6c4da1a3e5511af
Athul Raj <athulbraj@gmail.com>
2017-03-06 14:09:04 +0530

    Auto

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # TestingAPIsBot
 A Telegram Bot to test you API calls
+
+##The Bot has 2 purposes.
+
+1. Respond to a message from a user/group with all the info in the webhook
+1.1 Basically to get the chat_id
+
+2. Accept API calls.
+2.1 Subdomain is chat_id
+2.2 The entire request is sent as text.
+
+##Future
+
+1.Make this into an Amazon Lambda Endpoint if there is traffic.
---
+++ server.js
@@ -0,0 +1,68 @@
+var restify = require('restify');
+var plugins = require('restify-plugins');
+var request = require('request');
+var fs = require('fs');
+
+var settings = require('./settings.json');
+
+const server = restify.createServer({
+	name: 'TestingAPIsBot',
+	version: '0.1.1'
+});
+server.use(plugins.acceptParser(server.acceptable));
+server.use(plugins.queryParser());
+server.use(plugins.bodyParser());
+
+server.get('.*',fry);
+server.post('.*',fry);
+function sendTelegram(chat_id, text){
+	request.post(
+		{
+			url : 'https://api.telegram.org/bot'+settings.tBotToken+'/sendMessage',
+			formData : {
+					"chat_id":chat_id,
+					"text":text
+			}
+		},
+		function (e, r, body) {
+			//console.log(req);
+		}
+	);
+}
+function fry(req, res, next) {
+	if(req.headers['x-forwarded-host']){
+		try{
+			var result = {
+				"METHOD"	: req.method,
+				"PATH"		: req.getPath(),
+				"GET_VARS" 	: req.query,
+				"POST_VARS"	: req.body
+			}
+			sendTelegram(
+				req.headers['x-forwarded-host'].replace(/\:.*/,'').split('.')[0],
+				JSON.stringify(
+					result,
+					null,
+					7
+				)
+			);
+		}catch(e){
+			console.log(e);
+		}
+	}else{
+		try{
+			sendTelegram(
+				req.body.message.from.id,
+				"Use "+req.body.message.from.id+".testapi.xyz for all requests."
+			);
+		}catch(e){
+			console.log(e);
+		}
+	}
+	res.send(result);
+	return next();
+}
+
+server.listen(8080, function () {
+	console.log('%s listening at %s', server.name, server.url);
+});
